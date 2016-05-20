4139e60a243bd0921a98ee59b6035b1b3de98adf
Karen Ling <karensling@gmail.com>
2016-05-19 20:32:42 -0700

    Clean up.

--- frontend/OrganGrinder.jsx
+++ frontend/OrganGrinder.jsx
@@ -1,5 +1,4 @@
 Keys = require('./util/KeyListener');
-KeyStore = require('./stores/KeyStore'); // TODO cleanup
 var React = require('react');
 var ReactDOM = require('react-dom');
 var OrganKey = require('./components/OrganKey');
